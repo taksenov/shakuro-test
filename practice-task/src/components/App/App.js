2ef763e79c9f398c694b8c92d5015e25f2dc9d64
TAksenov <taksenov@gmail.com>
2018-02-13 16:21:03 +0500

    need functional for tabs steps

--- practice-task/src/components/App/App.js
+++ practice-task/src/components/App/App.js
@@ -1,45 +1,85 @@
 import React, { Component } from 'react';
 import MobilePaymentOperatorsGrid from '../MobilePaymentOperatorsGrid';
 import MobilePaymentForm from '../MobilePaymentForm';
-import { Container, Tab } from 'semantic-ui-react';
+import { Container, Tab, Message } from 'semantic-ui-react';
 
 import './App.css';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            activeIndex: 0,
+            paymentStepCondition: 0,
+            needMessage: true
+        };
+    } //constructor
+
+    handleTabChange = (e, { activeIndex }) => {
+        if (activeIndex === this.state.paymentStepCondition) {
+            this.setState({ activeIndex });
+        } else {
+            this.setState({ needMessage: false });
+            console.log(
+                'Условие для перехода на следующий шаг не выполнено',
+                activeIndex,
+                this.state.paymentStepCondition
+            );
+        }
+    }; //handleTabChange
+
     render() {
+        const { activeIndex } = this.state;
+
         const panes = [
             //step 1
             {
                 menuItem: 'Выбор оператора',
-                render: () => (
-                    <Tab.Pane>
-                        <MobilePaymentOperatorsGrid />
-                    </Tab.Pane>
-                )
+                render: () => {
+                    const needMessage = this.state.needMessage;
+                    return (
+                        <Tab.Pane>
+                            <MobilePaymentOperatorsGrid />
+                            <Message hidden={needMessage} warning>
+                                <Message.Header>Внимание!</Message.Header>
+                                <p>
+                                    Для перехода на следующий шаг необходимо
+                                    выбрать оператора
+                                </p>
+                            </Message>
+                        </Tab.Pane>
+                    );
+                }
             },
-
-            //step 2
             {
                 menuItem: 'Оплата',
                 render: () => (
                     <Tab.Pane>
+                        {' '}
+                        //step 2
                         <MobilePaymentForm />
                     </Tab.Pane>
                 )
             },
-
-            //step 3
             {
                 menuItem: 'Результат',
                 render: () => (
                     <Tab.Pane>
+                        {' '}
+                        //step 3
                         <MobilePaymentOperatorsGrid />
                     </Tab.Pane>
                 )
             }
         ];
 
-        const MenuTabs = () => <Tab panes={panes} />;
+        const MenuTabs = () => (
+            <Tab
+                panes={panes}
+                activeIndex={activeIndex}
+                onTabChange={this.handleTabChange}
+            />
+        );
 
         return (
             <div>
